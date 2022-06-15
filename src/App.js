347798ecd7f5b67c0e85ad03c12a3dec637007f2
AndresMiraQ <krhistian1902@gmail.com>
2022-06-15 16:20:59 -0500

    Fetching de datos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Note } from './components/Note';
    
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState(''); 
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => {
+        setNotes(json)
+        setLoading(true)});
+    }, 2000);
+  }, []); // empty array means run once on page load
   
   const handleChange = (event) => {
     setNewNote(event.target.value);
@@ -24,6 +35,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Notes</h1>
+      {loading ? '' : <p>Loading...</p>}
       <ol>
         {notes
         .map(note => <Note key={note.id} {...note} />)}
